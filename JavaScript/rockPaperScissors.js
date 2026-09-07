03313a6d2396dfee84c500dabb1bfe306a2d0701
Ralph Beentjes <ralphsimonbeentjes@gmail.com>
2026-09-07 10:56:00 +0200

    Added the userPlay function

--- JavaScript/rockPaperScissors.js
+++ JavaScript/rockPaperScissors.js
@@ -1,7 +1,7 @@
 function computerPlay(){
-    const computerChoice = '';
+    let computerChoice = '';
 
-    const randomNumber = Math.floor(Math.random() * 2);
+    const randomNumber = Math.floor(Math.random() * 3);
     
     if (randomNumber == 0){
         computerChoice = 'rock';
@@ -16,3 +16,28 @@ function computerPlay(){
     return computerChoice;
 }
 
+function userPlay(){
+    let userChoice = '';
+
+    let userInput = prompt('Please enter your weapon of choice: rock, paper or scissors').toLowerCase().trim();
+
+    switch(userInput){
+        case 'rock':
+            userChoice = 'rock';
+            console.log('You chose for the hardest option, a rock!')
+            break;
+        case 'paper':
+            userChoice = 'paper';
+            console.log('You chose the flimsy paper, will that work? Let\'s see!')
+            break;
+        case 'scissors':
+            userChoice = 'scissors';
+            console.log('You chose the sharp scissors, let\'s see if they can cut through!')
+            break;
+        default:
+            alert('Oops, you silly! That\'s not a correct input. Please enter rock, paper or scissors.');
+            userChoice = userPlay();
+    }
+
+    return userChoice;
+}
